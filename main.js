83036729f782a4c5ea20310cd08566556cdcd79c
Gabriel Lauro <103005846+FullStackFellow@users.noreply.github.com>
2024-11-21 09:27:36 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 // script.js
 
-// Function to move the image
-function moveImageOnScroll() {
-    const scrollPosition = window.pageYOffset; // Get the current scroll position
-    const image = document.getElementById('movingImage'); // Get the moving image element
-    
-    // Set the 'right' position of the image based on scroll position
-    image.style.right = scrollPosition * 0.5 + 'px'; // Adjust the multiplier for desired speed
-}
-
-// Add event listener to trigger the function on scroll
-window.addEventListener('scroll', moveImageOnScroll);
-
-
-
-
 function toggleNav() {
     const overlayWrapper = document.getElementById("navOverlayWrapper");
     if (overlayWrapper.style.display === "block") {
@@ -28,6 +13,4 @@ function toggleDropdown(id) {
     const dropdownMenu = document.getElementById(id);
     dropdownMenu.classList.toggle("show"); // Add or remove the 'show' class
 }
-
-
 
